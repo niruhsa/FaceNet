612600b352bd878eb35e0bddd6b8ccfc02b0b69b
niruhsa <>
2021-01-14 19:01:57 +1100

    Multi Worker stategy

--- src/network/autoencoder.py
+++ src/network/autoencoder.py
@@ -6,23 +6,25 @@ from network import encoder, decoder
 class AutoEncoder:
 
     def __new__(self):
-        self.input_shape = (128, 128, 1)
+        strategy = tf.distribute.MultiWorkerMirroredStrategy()
+        with mirrored_strategy.scope():
+            self.input_shape = (128, 128, 1)
 
-        self.image_input = tf.keras.layers.Input(shape = self.input_shape, name='input_image')
+            self.image_input = tf.keras.layers.Input(shape = self.input_shape, name='input_image')
 
-        self.encoder = encoder.Encoder()
-        self.decoder = decoder.Decoder(self.input_shape)
+            self.encoder = encoder.Encoder()
+            self.decoder = decoder.Decoder(self.input_shape)
 
-        self.enc = self.encoder(self.image_input)
-        
-        self.dec = self.decoder(self.enc)
+            self.enc = self.encoder(self.image_input)
+            
+            self.dec = self.decoder(self.enc)
 
-        self.model = tf.keras.Model(self.image_input, self.enc)
+            self.model = tf.keras.Model(self.image_input, self.enc)
+            self.model = tf.keras.models.load_model('data/model_original')
+
         self.model.compile(
             optimizer = tf.keras.optimizers.Adam(1e-3),
             loss = tfa.losses.TripletSemiHardLoss()
         )
-
-        self.model = tf.keras.models.load_model('data/models/2169')
 
         return self.model
